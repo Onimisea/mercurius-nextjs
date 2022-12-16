691d407dc13526698f6a0c0883ad5644ff00d813
Onimisea <onimisea@gmail.com>
2022-12-16 12:23:01 +0100

    Removed unused packages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
