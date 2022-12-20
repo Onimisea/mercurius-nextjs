ea291e2115806caed5ebbcc4f9666fcc5fe84ef6
Onimisea <onimisea@gmail.com>
2022-12-20 05:26:11 +0100

    Building a new flashsale products slider

--- components/FlashSlider.jsx
+++ components/FlashSlider.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const FlashSlider = () => {
   return (
-    <section className="w-full mx-auto bg-slate-500 grid place-items-center">
+    <section className="w-full mx-auto p-6 bg-slate-500 grid place-items-center">
       <h1 className="font-dalek text-3xl text-primary">
-        New Slider for Flashs Products
+        New Slider for Flash Products
       </h1>
       <p className="text-md text-gray-300">Construction In Progress</p>
     </section>
