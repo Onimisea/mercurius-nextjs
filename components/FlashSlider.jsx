b25ac7127c5e7aa75b12ac3ab1bee186dff8350c
Onimisea <onimisea@gmail.com>
2022-12-20 06:56:19 +0100

    Building new flash slider

--- components/FlashSlider.jsx
+++ components/FlashSlider.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { useAppContext } from "../context/AppContext";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const FlashSlider = () => {
   const { flashsaleProducts } = useAppContext();
 
   return (
-    <section className="w-full mx-auto p-6 bg-slate-500 grid place-items-center">
-      
-
+    <section className="w-full mx-auto p-6 bg-slate-500 grid place-items-center group relative">
       <section className="flex items-center justify-center overflow-hidden">
+        product
         {/* {flashsaleProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))} */}
+      </section>
+      <section className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 cursor-pointer">
+        <FaChevronLeft size={30} />
+      </section>
+
+      <section className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 cursor-pointer">
+        <FaChevronRight size={30} />
       </section>
     </section>
   );
