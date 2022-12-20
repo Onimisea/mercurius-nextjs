43dda40eed5e4ed3e5ff365d6461fa0be86fc2ba
Onimisea <onimisea@gmail.com>
2022-12-20 03:31:51 +0100

    Edited flashsale slider's settings

--- components/FlashsaleSlider.jsx
+++ components/FlashsaleSlider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { useAppContext } from "../context/AppContext";
 import "slick-carousel/slick/slick.css";
