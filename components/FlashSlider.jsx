be3732288cf21afc3d4f1cb7dda83de19eaa8ecc
Onimisea <onimisea@gmail.com>
2022-12-20 05:34:04 +0100

    Building a new flashsale products slider

--- components/FlashSlider.jsx
+++ components/FlashSlider.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import { useAppContext } from "../context/AppContext";
 
 const FlashSlider = () => {
+  const { flashsaleProducts } = useAppContext();
+
   return (
     <section className="w-full mx-auto p-6 bg-slate-500 grid place-items-center">
       <h1 className="font-dalek text-3xl text-primary">
         New Slider for Flash Products
       </h1>
       <p className="text-md text-gray-300">Construction In Progress</p>
+
+      <section className="">
+        {flashsaleProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
     </section>
   );
 };
--- components/Flashsales.jsx
+++ components/Flashsales.jsx
@@ -9,7 +9,7 @@ const Flashsales = () => {
   const { flashsaleTimerSwitch } = useAppContext();
   
   return (
-    <section className="w-[85%] mx-auto max-w-screen-xl">
+    <section className="w-[85%] mx-auto max-w-screen-xl relative">
       <section className="flex items-center justify-center bg-black px-5 py-3 sm2:w-fit mx-auto md:px-7 md:py-4 font-bold">
         <h1 className="text-white sm:text-lg md:text-2xl md2:text-3xl font-dalek">
           Flash Sale
@@ -17,7 +17,7 @@ const Flashsales = () => {
         {flashsaleTimerSwitch && <Timer />}
       </section>
 
-      <section className="my-6 ">
+      <section className="my-6 relative">
         <FlashSlider />
       </section>
     </section>
