8e9398605b3c0eb3cc5c145e60789c8d63629030
Onimisea <onimisea@gmail.com>
2022-12-20 09:13:29 +0100

    Building new flash slider

--- components/FlashSlider.jsx
+++ components/FlashSlider.jsx
@@ -8,9 +8,24 @@ const FlashSlider = () => {
 
   const slides = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  const slideLeft = () => {
+    const slider = document.getElementById("flashsaleSlider");
+
+    slider.scrollLeft = slider.scrollLeft + 260;
+  };
+
+  const slideRight = () => {
+    const slider = document.getElementById("flashsaleSlider");
+
+    slider.scrollLeft = slider.scrollLeft - 260;
+  };
+
   return (
     <section className="w-full mx-auto group relative overflow-hidden">
-      <section className="flex items-center whitespace-nowrap overflow-x-scroll scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-primary scrollbar-none">
+      <section
+        id="flashsaleSlider"
+        className="flex items-center whitespace-nowrap overflow-x-scroll scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-primary scrollbar-none"
+      >
         {/* {flashsaleProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))} */}
@@ -25,11 +40,17 @@ const FlashSlider = () => {
           );
         })}
       </section>
-      <section className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-0 text-2xl rounded-full p-2 cursor-pointer bg-primary text-white opacity-50 group-hover:opacity-100">
+      <section
+        className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-0 text-2xl rounded-full p-2 cursor-pointer bg-primary text-white opacity-50 group-hover:opacity-100"
+        onClick={slideLeft}
+      >
         <FaChevronLeft size={20} />
       </section>
 
-      <section className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-0 text-2xl rounded-full p-2 cursor-pointer bg-primary text-white opacity-50 group-hover:opacity-100">
+      <section
+        className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-0 text-2xl rounded-full p-2 cursor-pointer bg-primary text-white opacity-50 group-hover:opacity-100"
+        onClick={slideRight}
+      >
         <FaChevronRight size={20} />
       </section>
     </section>
