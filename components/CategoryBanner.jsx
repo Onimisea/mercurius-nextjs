6c28e19bd69a6139f1c0a0acfcd324824a8f7f50
Onimisea <onimisea@gmail.com>
2022-12-21 17:01:25 +0100

    Working on the category page

--- components/CategoryBanner.jsx
+++ components/CategoryBanner.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import Image from "next/image";
 
-const CategoryBanner = ({ catData }) => {
+const CategoryBanner = ({ catData, subcatData }) => {
   console.log(catData);
+  console.log(subcatData);
+
+  const bgUrl = (imgUrl) =>
+    "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
+
   return (
     <section className="w-full min-w-[320px] h-[100%] max-h-[400px] grid place-items-center overflow-hidden z-10 relative">
       {/* <section className="bg-black absolute top-0 left-0 w-full h-[100%] opacity-90"></section> */}
 
-      {/* <Image
-        src={catData.bg}
+      <Image
+        src={bgUrl(catData.category_image)}
         alt=""
         width={0}
         height={0}
         className="w-full h-[100%] max-h-[400px] object-cover object-center"
-      /> */}
+      />
 
       <section className="text-white absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-4xl uppercase font-poppins font-bold tracking-wider">
           {catData.name}
         </h1>
         <p className="my-5 tracking-wide text-lg">{catData.desc}</p>
-        <span className="text-primary">{`#mercurius${catData.name}`}</span>
+        <span className="text-primary">{`#mercurius${catData.slug}`}</span>
       </section>
     </section>
   );
--- pages/c/[...slug].js
+++ pages/c/[...slug].js
@@ -8,11 +8,9 @@ import { CategoryBanner } from "../../components";
 
 export default function CategoryPage({}) {
   const router = useRouter();
-
   const { products } = useAppContext();
-
-  const [catData, setCatData] = useState({});
   const [catObj, setCatObj] = useState({});
+  const [subcatObj, setSubcatObj] = useState({});
 
   const bgUrl = (imgUrl) =>
     "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
@@ -24,50 +22,37 @@ export default function CategoryPage({}) {
       )
         .then((res) => res.json())
         .then((catData) => {
-          const cat = catData.filter((category) => category.slug === router.query.slug[0])
+          const cat = catData.filter(
+            (category) => category.slug === router.query.slug[0]
+          );
 
           setCatObj(cat[0]);
         });
+      
+        const subcategories = fetch(
+        "https://mercurius-api-production.up.railway.app/api/inventory/c/"
+      )
+        .then((res) => res.json())
+        .then((catData) => {
+          const subcat = catData.filter(
+            (subcategory) => subcategory.subcategories.slug === router.query.slug[1]
+          );
 
-      console.log(router.query);
-
-      // console.log(catData);
+          setSubcatObj(subcat[0]);
+        });
     }
   }, []);
+
+  console.log(router.query);
   
-  console.log(catObj);
-
-  // const getCatData = () => {
-  //   // const cat = JSON.parse(window.localStorage.getItem("CategoryData"));
-  //   const ccd = categories.filter((category) => category.slug === router.query.slug);
-
-  //   console.log(ccd);
-  //   // console.log(router.query.slug);
-  //   const catExt = {
-  //     name: ccd[0].name,
-  //     desc: ccd[0].description,
-  //     slug: ccd[0].slug,
-  //     bg: bgUrl(ccd[0].category_image),
-  //   };
-
-  //   setCatObj(catExt);
-  // };
-
-  // useEffect(() => {
-  //   if (router.isReady) getCatData();
-  // }, [router.query, router.isReady]);
-
-  // console.log(catObj)
-
   return (
     <section className="">
       <Head>
-        <title>Mercurius | Best Thrift Store in Nigeria</title>
+        <title>Mercurius {catObj.name} | Best Thrift Store in Nigeria</title>
       </Head>
 
       <section className="">
-        <h1>Category Page</h1>
-        {/* <CategoryBanner catData={catObj} /> */}
+        <CategoryBanner catData={catObj} subcatData={subcatObj} />
       </section>
     </section>
   );
