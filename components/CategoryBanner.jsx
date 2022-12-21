71841f98d034cfeea3dc60be49f21bb3a1ab044e
Onimisea <onimisea@gmail.com>
2022-12-21 17:23:54 +0100

    Working on the category page

--- components/CategoryBanner.jsx
+++ components/CategoryBanner.jsx
@@ -8,17 +8,19 @@ const CategoryBanner = ({ catData }) => {
   const bgUrl = (imgUrl) =>
     "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
 
+  const catBg = bgUrl(catData.category_image);
+
   return (
     <section className="w-full min-w-[320px] h-[100%] max-h-[400px] grid place-items-center overflow-hidden z-10 relative">
       {/* <section className="bg-black absolute top-0 left-0 w-full h-[100%] opacity-90"></section> */}
 
-      {/* <Image
-        src={bgUrl(catData.category_image)}
+      <Image
+        src={catBg}
         alt=""
         width={0}
         height={0}
         className="w-full h-[100%] max-h-[400px] object-cover object-center"
-      /> */}
+      />
 
       {/* <section className="text-white absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-4xl uppercase font-poppins font-bold tracking-wider">
