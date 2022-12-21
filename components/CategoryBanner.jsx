1d926d3bcaee75661e151319342c8bd423cbe304
Onimisea <onimisea@gmail.com>
2022-12-21 21:02:12 +0100

    Working on the category page

--- components/CategoryBanner.jsx
+++ components/CategoryBanner.jsx
@@ -15,13 +15,14 @@ const CategoryBanner = ({ catData }) => {
   useEffect(() => {
     if (catData) {
       // setcatImg();
-      console.log(catData);
       // const catBg = bgUrl(catData.category_image);
       // console.log(catBg);
-      console.log(catData.category_image);
+      console.log(catData);
     }
+    
+    console.log(catData);
+    console.log(catData.category_image);
   }, []);
-  
 
   return (
     <section className="w-full min-w-[320px] h-[100%] max-h-[400px] grid place-items-center overflow-hidden z-10 relative">
