337c0ce8c013b07e9ede632623e3041548a67b1b
Onimisea <onimisea@gmail.com>
2022-12-21 17:18:52 +0100

    Working on the category page

--- components/CategoryBanner.jsx
+++ components/CategoryBanner.jsx
@@ -12,21 +12,21 @@ const CategoryBanner = ({ catData }) => {
     <section className="w-full min-w-[320px] h-[100%] max-h-[400px] grid place-items-center overflow-hidden z-10 relative">
       {/* <section className="bg-black absolute top-0 left-0 w-full h-[100%] opacity-90"></section> */}
 
-      <Image
+      {/* <Image
         src={bgUrl(catData.category_image)}
         alt=""
         width={0}
         height={0}
         className="w-full h-[100%] max-h-[400px] object-cover object-center"
-      />
+      /> */}
 
-      <section className="text-white absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+      {/* <section className="text-white absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
         <h1 className="text-4xl uppercase font-poppins font-bold tracking-wider">
           {catData.name}
         </h1>
         <p className="my-5 tracking-wide text-lg">{catData.desc}</p>
         <span className="text-primary">{`#mercurius${catData.slug}`}</span>
-      </section>
+      </section> */}
     </section>
   );
 };
