d14de1f9de059e1b2e553ff9d7857dcfa5cc7c7f
Onimisea <onimisea@gmail.com>
2022-12-22 02:07:39 +0100

    Working on the category page

--- pages/c/[...slug].js
+++ pages/c/[...slug].js
@@ -54,13 +54,13 @@ export default function CategoryPage({}) {
           // setCatObj(cat[0]);
           // setCatBg(bgUrl(cat[0].category_image));
 
-          // console.log(prodSubFilt);
+          console.log(prodSubFilt);
 
-          const prodLowerSubFilt = prodSubFilt[0].lowersubcategories.filter(
-            (lowersubcat) => lowersubcat.slug === router.query.slug[2]
-          );
+          // const prodLowerSubFilt = prodSubFilt[0].lowersubcategories.filter(
+          //   (lowersubcat) => lowersubcat.slug === router.query.slug[2]
+          // );
 
-          console.log(prodLowerSubFilt);
+          // console.log(prodLowerSubFilt);
 
           // setSubcatObj(subcats[0]);
         });
