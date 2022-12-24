3b4c1c048818955b913a1ebd410c9a40ed661325
Onimisea <onimisea@gmail.com>
2022-12-24 16:29:45 +0100

    Working on the Single Product Page

--- pages/[slug].js
+++ pages/[slug].js
@@ -77,20 +77,21 @@ const ProductPage = ({ products }) => {
             </section>
 
             <section className="w-full h-[150px] flex items-center justify-between">
-              {productDIs.map((pi) => {
-                console.log(pi);
+              {productDIs &&
+                productDIs.map((pi) => {
+                  console.log(pi);
 
-                // return (
-                //   <img
-                //     src={bgUrl(pi.product_images)}
-                //     // src={currProductDI}
-                //     alt={pi.product}
-                //     width={0}
-                //     height={0}
-                //     className="w-[150px] h-[150px] object-cover object-center z-20 mx-2"
-                //   />
-                // );
-              })}
+                  return (
+                    <img
+                      src={bgUrl(pi.product_images)}
+                      // src={currProductDI}
+                      alt={pi.product}
+                      width={0}
+                      height={0}
+                      className="w-[150px] h-[150px] object-cover object-center z-20 mx-2"
+                    />
+                  );
+                })}
             </section>
           </section>
 
