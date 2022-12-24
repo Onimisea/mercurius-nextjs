03e0a0827eb3b3ad74c60308589cbfa920313682
Onimisea <onimisea@gmail.com>
2022-12-24 14:33:30 +0100

    Working on the Single Product Page

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useAppContext } from "../context/AppContext";
 import Head from "next/head";
@@ -6,20 +6,39 @@ import Head from "next/head";
 const ProductPage = () => {
   const router = useRouter();
   const {
-    productFilter: { byCategory, searchQuery },
-    productFilterDispatch,
+    products,
+    appState: { cart, wishlist },
+    appStateDispatch,
+    addToCart,
+    removeFromCart,
+    addToWishlist,
+    removeFromWishlist,
+    totalPrice,
+    shipping,
+    setShipping,
+    salesTax,
+    setSalesTax,
+    numbersWithCommas,
+    increaseQty,
+    decreaseQty,
   } = useAppContext();
+
+  const [currProduct, setCurrProduct] = useState({});
 
   const bgUrl = (imgUrl) =>
     "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
 
+  useEffect(() => {
+    console.log(products)
+  }, [router.isReady]);
+
   return (
-    <section className="w-[85%] mx-auto max-w-screen-xl">
+    <section className="w-[85%] mx-auto max-w-screen-xl bg-red-500">
       <Head>
         <title>Mercurius | Best Thrift Store in Nigeria</title>
       </Head>
 
-      <section className="w-full mx-auto max-w-screen-xl flex flex-wrap items-center justify-center my-6">
+      <section className="w-full mx-auto max-w-screen-xl grid place-items-center my-6 bg-orange-500">
         <h1 className="text-black sm:text-2xl md:text-3xl md2:text-4xl lg:text-5xl font-dalek font-semibold">
           Single Product Page
         </h1>
