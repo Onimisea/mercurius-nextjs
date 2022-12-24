8d6ee57d1df8bb714b9e9e137800c83537903caf
Onimisea <onimisea@gmail.com>
2022-12-24 11:20:05 +0100

    Working on the Single Product Page

---
+++ pages/[slug].js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { useAppContext } from "../../context/AppContext";
+
+const ProductPage = () => {
+  const router = useRouter();
+  const {
+    productFilter: { byCategory, searchQuery },
+    productFilterDispatch,
+  } = useAppContext();
+
+  const bgUrl = (imgUrl) =>
+    "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
+
+  return (
+    <section className="w-[85%] mx-auto max-w-screen-xl">
+      <Head>
+        <title>Mercurius | Best Thrift Store in Nigeria</title>
+      </Head>
+
+      <section className="w-full mx-auto max-w-screen-xl flex flex-wrap items-center justify-center mb-6">
+        <h1>Single Product Page</h1>
+      </section>
+    </section>
+  );
+};
+
+export default ProductPage;
