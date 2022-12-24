f929b9b4fd3fe69c4f2fec5492d19145056434a9
Onimisea <onimisea@gmail.com>
2022-12-24 14:50:07 +0100

    Working on the Single Product Page

--- pages/[slug].js
+++ pages/[slug].js
@@ -29,11 +29,15 @@ const ProductPage = () => {
     "https://res.cloudinary.com/dxhq8jlxf/" + imgUrl.replace(/ /g, "%20");
 
   useEffect(() => {
-    console.log(products)
-    console.log(router.query)
-    // const currProd = products.filter((prod) => prod.slug === router.query.slug)
+    console.log(products);
+    console.log(router.query.slug);
 
+    const currProd = products.filter((prod) => prod.slug === router.query.slug);
+
+    setCurrProduct(currProd);
   }, [router.query]);
+
+  console.log(currProduct);
 
   return (
     <section className="w-[85%] mx-auto max-w-screen-xl">
@@ -41,10 +45,26 @@ const ProductPage = () => {
         <title>Mercurius | Best Thrift Store in Nigeria</title>
       </Head>
 
-      <section className="w-full mx-auto max-w-screen-xl grid place-items-center my-6 bg-orange-500">
-        <h1 className="text-black sm:text-2xl md:text-3xl md2:text-4xl lg:text-5xl font-dalek font-semibold">
-          Single Product Page
-        </h1>
+      <section className="w-full mx-auto max-w-screen-xl grid place-items-center my-6 bg-white">
+        <section className="">
+          <section className="">
+            <h1 className="text-black sm:text-2xl md:text-3xl md2:text-4xl font-dalek font-semibold">
+              Product Images
+            </h1>
+          </section>
+
+          <section className="">
+            <h1 className="text-black sm:text-2xl md:text-3xl md2:text-4xl font-dalek font-semibold">
+              Product Details
+            </h1>
+          </section>
+        </section>
+
+        <section className="">
+          <h1 className="text-black sm:text-2xl md:text-3xl md2:text-4xl font-dalek font-semibold">
+            You Might Also Like
+          </h1>
+        </section>
       </section>
     </section>
   );
