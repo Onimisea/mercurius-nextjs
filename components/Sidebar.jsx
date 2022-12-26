da953996d65cf70042e5e229992d4a1cbeb7b900
Onimisea <onimisea@gmail.com>
2022-12-26 15:40:25 +0100

    Working on the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,15 +5,15 @@ const Sidebar = ({ links }) => {
   console.log(links);
   return (
     <section className="w-full h-fit grid place-items-center">
-      <ul className="p-0 m-0 block w-full space-y-4 bg-red-300">
+      <ul className="p-0 m-0 block w-full space-y-4">
         {links.map((ln) => (
-          <li className="w-full p-2 md:text-md bg-red-600 grid place-items-center">
+          <li className="w-full p-2 md:text-md grid place-items-center">
             <Link
               href={ln.url}
-              className={`w-[80%] flex items-center justify-start h-full cursor-pointer text-white hover:text-primary bg-red-800`}
+              className={`w-[80%] flex items-start justify-start h-full cursor-pointer text-white hover:text-primary bg-red-200`}
             >
-              <span className="w-[30%]">{ln.icon}</span>
-              <span className="w-[60%]">{ln.name}</span>
+              <span className="w-[30%] bg-red-400">{ln.icon}</span>
+              <span className="w-[60%] bg-red-500">{ln.name}</span>
             </Link>
           </li>
         ))}
