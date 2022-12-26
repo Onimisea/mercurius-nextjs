d702c031f6c10d3585380ed1fe6093e3f10151c8
Onimisea <onimisea@gmail.com>
2022-12-26 17:22:39 +0100

    Working on the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = ({ links }) => {
     <section className="w-full h-fit flex items-center justify-center">
       <ul className="p-0 m-0 flex flex-col items-center justify-center w-full space-y-4">
         {links.map((ln) => (
-          <li className="bg-red-400 w-full md:text-md flex flex-col items-start justify-start">
+          <li className="w-fit md:text-md flex flex-col items-start justify-start">
             <Link
               href={ln.url}
               className={`bg-red-800 w-[60%] flex items-center justify-start h-full cursor-pointer text-white hover:text-primary`}
