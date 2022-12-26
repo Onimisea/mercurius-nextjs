4d810b4230e492206ad18b62f6b9015b1fb950cb
Onimisea <onimisea@gmail.com>
2022-12-26 18:45:23 +0100

    Working on the account page

--- pages/account.js
+++ pages/account.js
@@ -87,7 +87,7 @@ const account = ({ userStatus }) => {
           </h1>
 
           <section className="w-full flex items-start justify-between mt-8">
-            <section className="bg-black w-[200px] md:px-[18px] md:py-[35px] grid place-items-center">
+            <section className="bg-black w-[200px] sm2:w-[250] md:px-[20px] md:py-[35px] grid place-items-center">
               <Sidebar links={sidebarLinks} />
               {/* md2:py-6 md2:px-8 md3:py-8 md3:px-12 lg:py-12 lg:px-16 */}
             </section>
