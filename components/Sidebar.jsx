614b4ec231898dbc89a0e22a0141354c44a1f074
Onimisea <onimisea@gmail.com>
2022-12-26 17:08:06 +0100

    Working on the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 const Sidebar = ({ links }) => {
 
   return (
-    <section className="bg-red-200 w-full h-fit flex items-center justify-center">
-      <ul className="bg-red-400 p-0 m-0 flex flex-col items-center justify-center w-full space-y-4">
+    <section className="w-full h-fit flex items-center justify-center">
+      <ul className="p-0 m-0 flex flex-col items-center justify-center w-full space-y-4">
         {links.map((ln) => (
-          <li className="bg-red-600 w-full p-2 md:text-md flex flex-col items-center justify-center">
+          <li className="bg-red-400 w-full p-2 md:text-md flex flex-col items-center justify-center">
             <Link
               href={ln.url}
               className={`bg-red-800 w-[60%] flex items-center justify-start h-full cursor-pointer text-white hover:text-primary`}
