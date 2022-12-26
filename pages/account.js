0de40157fdede0e1d8120abdf1b5cf92b680ec89
Onimisea <onimisea@gmail.com>
2022-12-26 13:34:14 +0100

    Working on the account page

--- pages/account.js
+++ pages/account.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const account = () => {
   return (
@@ -7,9 +7,19 @@ const account = () => {
         <title>Mercurius | Account | Best Thrift Store in Nigeria</title>
       </Head>
 
-      <section className="flex flex-col my-10 lg:flex-row lg:items-start lg:justify-between lg:my-16"></section>
+      <section className="w-full grid place-items-center my-10">
+        <h1 className="text-2xl sm2:text-3xl md2:text-4xl text-primary font-dalek">
+          Settings
+        </h1>
+
+        <section className="w-full flex items-start justify-between mt-8">
+          <section className="bg-gray-300 w-[36%]">Sidebar</section>
+
+          <section className="bg-gray-400 w-[60%]">Main</section>
+        </section>
+      </section>
     </section>
   );
-}
+};
 
-export default account
+export default account;
