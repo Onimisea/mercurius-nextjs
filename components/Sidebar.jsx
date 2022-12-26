67e9706cdbb9101b40f9560271676d97575f0ed0
Onimisea <onimisea@gmail.com>
2022-12-26 22:18:26 +0100

    Working on the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({ links, asideOpen }) => {
                 asideOpen ? "w-[150px] ml-4" : "w-[60px]"
               } sm2:w-[150px] flex items-center justify-start h-full cursor-pointer text-white hover:text-primary ${
                 ln.active && "text-primary hover:text-gray-300"
-              } duration-300 bg-red-400`}
+              } duration-300`}
             >
               <span className="mr-1">{ln.icon}</span>
               <span
