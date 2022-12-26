04e95e8eb7ef3456578b1d706d644e17560ce00b
Onimisea <onimisea@gmail.com>
2022-12-26 18:04:51 +0100

    Working on the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 const Sidebar = ({ links }) => {
 
   return (
-    <section className="w-full h-fit flex items-center justify-center">
+    <section className="w-full h-fit flex items-center justify-center bg-red-300">
       <ul className="p-0 m-0 flex flex-col items-center justify-center w-full space-y-4">
         {links.map((ln) => (
-          <li className="w-full md:text-md flex flex-col items-start justify-start">
+          <li className="w-full md:text-md flex flex-col items-start justify-start bg-red-500">
             <Link
               href={ln.url}
-              className={`w-full flex items-center justify-start h-full cursor-pointer text-white hover:text-primary ${ln.active && "text-primary hover:text-gray-500"}`}
+              className={`w-full flex items-center justify-start h-full cursor-pointer text-white hover:text-primary ${
+                ln.active && "text-primary hover:text-gray-500"
+              } bg-red-700`}
             >
               <span className="mr-1">{ln.icon}</span>
               <span className="">{ln.name}</span>
