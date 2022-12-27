39d3dd164ecfa26c120846002268823f0707fedc
Onimisea <onimisea@gmail.com>
2022-12-27 09:34:27 +0100

    Working on the functionality of the account page

--- pages/account.js
+++ pages/account.js
@@ -55,7 +55,7 @@ const account = ({ userStatus }) => {
     },
     {
       name: "Log Out",
-      url: "",
+      url: null,
       icon: <RiLogoutBoxFill size={20} className="mr-2" />,
     },
   ];
