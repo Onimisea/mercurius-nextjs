9fd9932018758bb09870b43c56022b4eb48b17b5
Onimisea <onimisea@gmail.com>
2022-12-27 16:49:12 +0100

    Working on the functionality of the account page

--- pages/account/account-details.js
+++ pages/account/account-details.js
@@ -186,7 +186,7 @@ const AccountDetails = ({ userStatus }) => {
                 } overflow-x-hidden scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-primary scroll-smooth space-y-3 duration-500`}
               >
                 <form
-                  className="grid grid-cols-1 grid-rows-3 gap-4 sm2:grid-cols-2 space-y-6 w-full"
+                  className="grid grid-cols-1 grid-rows-3 gap-4 sm2:grid-cols-2 space-y-0 w-full"
                   onSubmit={handleSubmit(onSubmit)}
                 >
                   <section className="col-span-2 sm2:col-span-full">
@@ -319,7 +319,7 @@ const AccountDetails = ({ userStatus }) => {
                     )}
                   </section>
 
-                  <button className="row-start-3 bg-black text-white rounded-sm px-6 py-3 grid place-items-center cursor-pointer hover:bg-primary duration-300">
+                  <button className="col-start-2 bg-black text-white rounded-sm px-6 py-3 grid place-items-center cursor-pointer hover:bg-primary duration-300">
                     <span>Save Changes</span>
                   </button>
                 </form>
