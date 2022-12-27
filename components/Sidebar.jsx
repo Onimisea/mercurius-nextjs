c51c14b4cd19202baf6ad148aa18fde95ba5cff3
Onimisea <onimisea@gmail.com>
2022-12-27 09:42:44 +0100

    Working on the functionality of the account page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = ({ links, asideOpen }) => {
           return (
             <li className="w-fit md:text-md flex flex-col items-start justify-start">
               <Link
-                href=""
+                href={ln.url && ln.url}
                 className={` ${
                   asideOpen ? "w-[150px]" : "w-[60px]"
                 } sm2:w-[150px] flex items-center justify-start h-full cursor-pointer text-white hover:text-primary ${
