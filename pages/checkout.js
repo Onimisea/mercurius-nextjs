f4838f4802d5349f50d5dfcc379566b0816ae39f
Onimisea <onimisea@gmail.com>
2023-01-12 04:14:51 +0100

    Debugging checked user

--- pages/checkout.js
+++ pages/checkout.js
@@ -25,9 +25,6 @@ const Checkout = ({ userStatus }) => {
 
   const { data: session } = useSession();
 
-  console.log("session", session);
-  console.log("userStatus", userStatus);
-
   const handleSignOut = () => {
     window.localStorage.removeItem("UserData");
     setUserInfo(null);
