a99b88ccc2316ab916e086e0f3295e3d433fe515
Onimisea <onimisea@gmail.com>
2023-01-12 09:04:39 +0100

    Working on the orders page

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { useAppContext } from "../context/AppContext";
+import { useAppContext } from "../../context/AppContext";
 import toast from "react-hot-toast";
 import { getSession, useSession, signOut } from "next-auth/react";
-import { Sidebar2 } from "../components";
+import { Sidebar2 } from "../../components";
 import { FiMenu, FiPackage, FiEdit } from "react-icons/fi";
 import { FaEnvelope, FaHeart } from "react-icons/fa";
 import { MdClose, MdInventory } from "react-icons/md";
